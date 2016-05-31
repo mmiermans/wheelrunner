ec49ce84b09c9e3a229636dfd67e321814556e3c
Matt <Matt Miermans>
2016-05-31 10:26:01 -0700

    implemented 'hello world' telegram bot and config file

---
+++ wheelrunner/wheelrunner.py
@@ -0,0 +1,35 @@
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    Filters,
+    )
+import logging
+import config
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                            level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+job_queue = None
+
+def start(bot, update):
+    bot.sendMessage(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+
+def echo(bot, update):
+    bot.sendMessage(chat_id=update.message.chat_id, text=update.message.text)
+
+def main():
+    # start Telegram bot
+    updater = Updater(token=config.token)
+    job_queue = updater.job_queue
+    dispatcher = updater.dispatcher
+
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
+    echo_handler = MessageHandler([Filters.text], echo)
+    updater.start_polling()
+
+if __name__ == "__main__":
+    main()
